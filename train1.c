e3eba28040dcbb3cb06e67821e1fb1d2069f85ed
Tim Hsu <tim37021@gmail.com>
2013-11-26 02:15:58 +0800

    Add the answer of train1

--- train1.c
+++ train1.c
@@ -20,6 +20,14 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+/* Bonus 1 */
+static void mousepos_callback(GLFWwindow *window, double x, double y){
+	char buf[50];
+	sprintf(buf, "Chapter One - Your First Window (%.2f, %.2f)", x, y);
+	glfwSetWindowTitle(window, buf);
+}
+/**********/
 int main(void)
 {
     GLFWwindow* window;
@@ -34,6 +42,11 @@ int main(void)
     }
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
+    
+    /* Bonus 1 */
+    glfwSetCursorPosCallback(window, mousepos_callback);
+    /***********/
+
     while (!glfwWindowShouldClose(window))
     {
         int width, height;
